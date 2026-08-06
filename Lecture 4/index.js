419bb0b532daffd05af2602f663102d2d9e3dac2
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-08-06 13:25:03 +0530

    done

--- Lecture 4/index.js
+++ Lecture 4/index.js
@@ -1,6 +1,12 @@
-const math = require('./math.js');
+// const math = require('./math.js');
+import {add, sub, mul, div} from './math.js';
 
-console.log(math.add(2, 3));
-console.log(math.sub(2, 3));
-console.log(math.mul(2, 3));
-console.log(math.div(2, 3));
+// console.log(math.add(2, 3));
+// console.log(math.sub(2, 3));
+// console.log(math.mul(2, 3));
+// console.log(math.div(2, 3));
+
+console.log(add(2, 3));
+console.log(sub(2, 3));
+console.log(mul(2, 3));
+console.log(div(2, 3));
--- Lecture 4/math.js
+++ Lecture 4/math.js
@@ -1,17 +1,17 @@
-function add (a,b) {
+export function add (a,b) {
     return a + b;
 }
 
-function sub (a,b) {
+export function sub (a,b) {
     return a - b;
 }
 
-function mul (a,b) {
+export function mul (a,b) {
     return a * b;
 }
 
-function div (a,b) {
+export function div (a,b) {
     return a / b;
 }
 
-module.exports = { add, sub, mul, div };
+// module.exports = { add, sub, mul, div };
