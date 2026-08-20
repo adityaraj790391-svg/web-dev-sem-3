6bf7eb5c82f50cbccef8f89f78312b388f1ac142
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-08-20 14:09:09 +0530

    done

--- Lecture 9/index.js
+++ Lecture 9/index.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
+
+
+// app.get('/', (req, res) => {
+//   res.status(200).send('Heavenly Demon')
+// })
+
+//  CRUD OPERATIONS
+
+// READ 
+let students = ["Vikir", "Cheon Ma", "Lee Geon"]
+
+app.get('/student', (req,res) => {
+  res.status(200).send(students);
+})
+
+// CREATE
+
+app.post('/student', (req,res) => {
+  let data = req.body.name
+  students.push(data);
+  res.status(200).send(" The Demon God ");
+})
+
+// PUT - update
+
+app.put('/student/:id', (req,res) => {
+  console.log("server is running on http://localhost:3000");
+  let id = req.params.id
+  let data = req.body.name
+
+  students[id] = data;
+  res.status(200).send("Ascended");
+})
+
 
 app.listen(3000, () => {
   console.log("server is running on http://localhost:3000")
