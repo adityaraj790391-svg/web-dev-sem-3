04732ba01fa3f1ee9433cd8473da347274e63c30
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-09-02 13:49:45 +0530

    done

---
+++ Lecture 13/models/data.js
@@ -0,0 +1,34 @@
+let notes  = [
+  {
+    id:1,
+    title: "Web DEV III",
+    description: "This is web dev III notes",
+    note: "app.js",
+    createdAt: "2026-06-10",
+    author: "DORGO"
+  },
+  {
+    id:2,
+    title: "OS",
+    description: "This is OS notes",
+    note: "app.js",
+    createdAt: "2026-01-10",
+    author: "VIKIR"
+  },
+  {
+    id:3,
+    title: "DBMS",
+    description: "This is DBMS notes",
+    note: "app.js",
+    createdAt: "2026-02-10",
+    author: "ARIANA"
+  },
+  {
+    id:4,
+    title: "Martial Arts",
+    description: "This is martial arts notes",
+    note: "app.js",
+    createdAt: "2026-03-10",
+    author: "LEE GEON"
+  }
+]
