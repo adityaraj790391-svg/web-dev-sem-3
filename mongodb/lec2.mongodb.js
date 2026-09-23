c8f0729424a22c5352e7e86baf0773071dcd6321
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-09-23 14:09:22 +0530

    done

--- mongodb/lec2.mongodb.js
+++ mongodb/lec2.mongodb.js
@@ -3,4 +3,9 @@ use("AiMl")
 // to find count of retreiving documents
 // db.students.find().count()
 
-db.students.find().skip(3).limit(5)
+// db.students.find().skip(3).limit(5)
+
+db.students.find({},{_id:0, studentId:1, name:1, course:1});
+
+db.students.find({},{_id:0, attendance:1, name:1, age:1, marks:1, course:1});
+
