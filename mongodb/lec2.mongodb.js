0d63dcab08e22149bca196c8f47155080c2efb8b
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-09-23 14:32:25 +0530

    done

--- mongodb/lec2.mongodb.js
+++ mongodb/lec2.mongodb.js
@@ -7,5 +7,11 @@ use("AiMl")
 
 db.students.find({},{_id:0, studentId:1, name:1, course:1});
 
-db.students.find({},{_id:0, attendance:1, name:1, age:1, marks:1, course:1});
 
+db.students.find({'age':{$lte:18}},{_id:0, attendance:1, name:1, age:1, marks:1, course:1});
+
+
+// to find the range doc
+db.students.find({'attendance':{$gte:80, $lte:90}})
+
+// 
